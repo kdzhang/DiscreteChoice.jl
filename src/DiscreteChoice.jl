3e2d53ed81408ae1e77875312e38ac0837a3b556
Kaida Zhang <zhangkaida.xjtu@gmail.com>
2021-03-11 12:11:29 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.15

---
+++ src/DiscreteChoice.jl
@@ -0,0 +1,5 @@
+module DiscreteChoice
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DiscreteChoice
+using Test
+
+@testset "DiscreteChoice.jl" begin
+    # Write your tests here.
+end
